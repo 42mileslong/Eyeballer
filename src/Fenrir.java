98fc455fcd8d5ca831d8a315c901c98d77e65fbb
TPackard <tyler.packard86@gmail.com>
2014-04-29 11:33:47 -0700

    Added robot to fenrir

--- src/Fenrir.java
+++ src/Fenrir.java
@@ -1,5 +1,26 @@
+import java.awt.*;
+
 /**
  * Runs the Robot class
  */
 public class Fenrir {
+	Robot fenrir;
+
+	private static Robot newRobot() {
+		try {
+			return new Robot();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static Robot newRobot(GraphicsDevice screen) {
+		try {
+			return new Robot(screen);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
